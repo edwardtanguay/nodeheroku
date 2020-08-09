2fc8d8ab077975232bff678d270e4f42337be958
Edward Tanguay (hm.win) <edward@tanguay.info>
2020-08-09 14:27:02 +0200

    customized

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3001;
 
 app.use('/', (req, res) => {
-	res.send('this is a node app');
+	res.send('Welcome to this custom node app.');
 });
 app.listen(process.env.PORT || 5000, () => {
 	console.log(`Listening at: http://localhost:${port}`);
